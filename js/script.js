6d5e3f224265b268d6354b3cebb07f5b59dc59fb
BrunoGaruda <bruleorn@gmail.com>
2022-06-17 16:47:34 -0300

    fazendo o Smooth scrolling no Javascript juntamente com o desaparecimento da barra de navegação ao clicar

--- js/script.js
+++ js/script.js
@@ -18,6 +18,37 @@ const currentYear = new Date().getFullYear()
 yearEl.textContent = currentYear
 
 ///////////////////////////////////////////////////////////
+// Smooth scrolling animation
+
+const allLinks = document.querySelectorAll('a:link')
+
+allLinks.forEach(function (link) {
+  link.addEventListener('click', function (e) {
+    e.preventDefault()
+    const href = link.getAttribute('href')
+    console.log(href)
+
+    //Scroll back to top
+    if (href === '#')
+      window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+      })
+
+    //Scrol to other links
+    if (href !== '#' && href.startsWith('#')) {
+      const sectionEl = document.querySelector(href)
+      sectionEl.scrollIntoView({ behavior: 'smooth' })
+    }
+
+    //Close mobile navigation
+    if (link.classList.contains('main-nav-link')) {
+      headerEl.classList.toggle('nav-open')
+    }
+  })
+})
+
+///////////////////////////////////////////////////////////
 // Make mobile navigation work
 
 const btnNavEl = document.querySelector('.btn-mobile-nav')
